fd60697ea4b3be3d1d945c0b3e33d1788964cb30
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-02-14 15:41:52 +0900

    trpc setup done.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,18 @@
+import { HydrateClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+export default async function Home() {
+  // サーバーコンポーネントでprefetchを行う→クライアントコンポーネントでuseSuspenseQueryを使う
+  void trpc.hello.prefetch({ text: 'client' });
 
-
-export default function Home() {
   return (
-    <p>
-      Home
-    </p>
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Error</div>}>
+          <p>
+          </p>
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
   );
 }
