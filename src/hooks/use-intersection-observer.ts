0bc4989684dd366ebfe3308f4fb5418fafade007
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-02-16 18:39:17 +0900

    infinite loading done.

---
+++ src/hooks/use-intersection-observer.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
+// TODO: テンプレ化
+export const useIntersectionObserver = (options?: IntersectionObserverInit) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const targetRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting);
+    }, options);
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [options]);
+
+  return {
+    targetRef,
+    isIntersecting,
+  }
+}
