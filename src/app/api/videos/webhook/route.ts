8e9dfd9045d9f622750c5a2779ff70a113ab1fa0
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-02-17 19:15:07 +0900

    Mux Webhooks done.

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -6,6 +6,7 @@ import {
   VideoAssetErroredWebhookEvent,
   VideoAssetReadyWebhookEvent,
   VideoAssetTrackReadyWebhookEvent,
+  VideoAssetDeletedWebhookEvent,
 } from "@mux/mux-node/resources/webhooks";
 import { eq } from "drizzle-orm";
 
@@ -17,7 +18,8 @@ type WebhookEvent =
   | VideoAssetCreatedWebhookEvent
   | VideoAssetErroredWebhookEvent
   | VideoAssetReadyWebhookEvent
-  | VideoAssetTrackReadyWebhookEvent;
+  | VideoAssetTrackReadyWebhookEvent
+  | VideoAssetDeletedWebhookEvent;
 
 export const POST = async (request: Request) => {
   if (!SIGNING_SECRET) {
@@ -57,6 +59,90 @@ export const POST = async (request: Request) => {
           muxStatus: data.status
         })
         .where(eq(videos.muxUploadId, data.upload_id));
+
+      break;
+    }
+    case "video.asset.ready": {
+      const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+      const playbackId = data.playback_ids?.[0]?.id;
+
+      if (!data.upload_id) {
+        return new Response("No upload id found", { status: 401 });
+      }
+
+      if (!playbackId) {
+        return new Response("No playback id found", { status: 401 });
+      }
+
+      const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.png`;
+      const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
+
+      const duration = data.duration ? Math.round(data.duration / 1000) : 0;
+
+      await db
+        .update(videos)
+        .set({
+          muxStatus: data.status,
+          muxPlaybackId: playbackId,
+          muxAssetId: data.id,
+          thumbnailUrl,
+          previewUrl,
+          duration,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+
+      break;
+    }
+    case "video.asset.errored": {
+      const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+
+      if (!data.upload_id) {
+        return new Response("No upload id found", { status: 401 });
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxStatus: data.status,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+
+      break;
+    }
+    case "video.asset.deleted": {
+      const data = payload.data as VideoAssetDeletedWebhookEvent["data"];
+
+      if (!data.upload_id) {
+        return new Response("No upload id found", { status: 401 });
+      }
+
+      await db
+        .delete(videos)
+        .where(eq(videos.muxUploadId, data.upload_id));
+
+      break;
+    }
+
+    case "video.asset.track.ready": {
+      const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+        asset_id: string;
+      };
+
+      const assetId = data.asset_id;
+      const trackId = data.id;
+      const status = data.status;
+
+      if (!assetId) {
+        return new Response("No asset id, track id or status found", { status: 401 });
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxTrackId: trackId,
+          muxTrackStatus: status,
+        })
+        .where(eq(videos.muxAssetId, assetId));
 
       break;
     }
---
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -0,0 +1,43 @@
+import { formatDuration } from "@/lib/utils"
+import Image from "next/image"
+
+interface VideoThumbnailProps {
+  title: string
+  imageUrl?: string | null
+  previewUrl?: string | null
+  duration: number
+}
+
+export const VideoThumbnail = ({
+  imageUrl,
+  previewUrl,
+  title,
+  duration,
+}: VideoThumbnailProps) => {
+  return (
+    // Thumbnail wrapper
+    <div className="relative group">
+      <div className="relative w-full overflow-hidden rounded-xl aspect-video">
+        <Image
+          unoptimized={!!previewUrl}
+          src={imageUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="size-full object-cover group-hover:opacity-0"
+        />
+        <Image
+          unoptimized={!!previewUrl}
+          src={previewUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="size-full object-cover opacity-0 group-hover:opacity-100"
+        />
+      </div>
+      {/* // Video duration box */}
+      <div className="absolute bottom-2 right-2 px-1 py-0.5 bg-black/80 text-white text-xs font-medium">
+        {formatDuration(duration)}
+      </div>
+
+    </div>
+  )
+}
