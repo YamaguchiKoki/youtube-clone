da623f03f72607c16e604bd7578c3a537cb4249b
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-02-22 12:33:46 +0900

    Comment Infinite Loading done.

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,11 +1,37 @@
 import { db } from "@/db";
 import { comments, users } from "@/db/schema";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { eq, getTableColumns } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
+import { and, eq, getTableColumns, desc, lt, or, count } from "drizzle-orm";
 import { z } from "zod";
 
 
 export const commentsRouter = createTRPCRouter({
+  remove: protectedProcedure
+    .input(z.object({
+      id: z.string().uuid(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+      const { id: userId } = ctx.user;
+
+      const [deletedComment] = await db
+        .delete(comments)
+        .where(and(
+          eq(comments.id, id),
+          eq(comments.userId, userId),
+        ))
+        .returning();
+
+      if (!deletedComment) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Comment not found",
+        });
+      }
+
+      return deletedComment;
+  }),
   create: protectedProcedure
     .input(z.object({
       videoId: z.string().uuid(),
@@ -25,23 +51,62 @@ export const commentsRouter = createTRPCRouter({
         .returning();
 
       return createdComment;
-    }),
+  }),
   getMany: baseProcedure
     .input(z.object({
       videoId: z.string().uuid(),
+      cursor: z.object({
+        id: z.string().uuid(),
+        updatedAt: z.date(),
+      }).nullish(),
+      limit: z.number().min(1).max(100).default(10),
     }))
     .query(async ({ input }) => {
-      const { videoId } = input;
+      const { videoId, cursor, limit } = input;
 
-      const data = await db
-        .select({
+      const [totalData, data] =  await Promise.all([
+        db
+          .select({
+            total: count(),
+          })
+          .from(comments)
+          .where(eq(comments.videoId, videoId)),
+        db
+          .select({
           ...getTableColumns(comments),
-          user: users
+          user: users,
         })
         .from(comments)
-        .where(eq(comments.videoId, videoId))
+        .where(and(
+          eq(comments.videoId, videoId),
+          cursor ?
+            or(
+              lt(comments.updatedAt, cursor.updatedAt),
+              and(
+                eq(comments.updatedAt, cursor.updatedAt),
+                lt(comments.id, cursor.id)
+              )
+            ) :
+            undefined))
         .innerJoin(users, eq(comments.userId, users.id))
+        .orderBy(desc(comments.updatedAt), desc(comments.id))
+        .limit(limit + 1),
+      ]);
 
-      return data;
+      const hasMore = data.length > limit;
+      const items = hasMore ? data.slice(0, -1) : data;
+
+      const lastItem = items[items.length - 1];
+
+      const nextCursor = hasMore ? {
+        id: lastItem.id,
+        updatedAt: lastItem.updatedAt,
+      } : null;
+
+      return {
+        total: totalData[0].total,
+        items,
+        nextCursor,
+      };
     }),
 });
--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,13 +1,35 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { UserAvatar } from "@/components/user-avatar";
 import { CommentGetManyOutput } from "@/modules/comments/type";
+import { trpc } from "@/trpc/client";
 import { formatDistanceToNow } from "date-fns";
+import { MessageSquareIcon, MoreVerticalIcon, TrashIcon } from "lucide-react";
 import Link from "next/link";
-
+import { Button } from "@/components/ui/button";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import { toast } from "sonner";
 interface CommentItemProps {
-  comment: CommentGetManyOutput[number];
+  comment: CommentGetManyOutput["items"][number];
 }
 
 export const CommentItem = ({ comment }: CommentItemProps) => {
+  const { userId } = useAuth();
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+  const remove = trpc.comments.remove.useMutation({
+    onSuccess: () => {
+      toast.success("Comment deleted");
+      utils.comments.getMany.invalidate();
+    },
+    onError: (error) => {
+      toast.error("Failed to delete comment");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    },
+  });
+
   return (
     <div>
       <div className="flex gap-4">
@@ -30,7 +52,27 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
             </div>
           </Link>
           <p className="text-sm">{comment.value}</p>
+          {/* TODO: Reactions */}
         </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon" className="size-8">
+              <MoreVerticalIcon />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => {}}>
+              <MessageSquareIcon className="size-4"/>
+              Reply
+            </DropdownMenuItem>
+            {comment.user.clerkId === userId && (
+              <DropdownMenuItem onClick={() => remove.mutate({ id: comment.id })}>
+                <TrashIcon className="size-4"/>
+                Delete
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
